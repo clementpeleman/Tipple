5446f8d32cedc83df7c02feebff471b253437df2
Clement Peleman <clementpeleman@outlook.com>
2025-02-24 16:54:53 +0100

    font size

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -48,9 +48,9 @@ function HeroPill() {
 
 function HeroTitles() {
   return (
-    <div className="flex w-full max-w-3xl flex-col space-y-4 overflow-hidden pt-8">
+    <div className="flex w-full max-w-6xl flex-col space-y-4 overflow-hidden pt-8">
       <motion.h1
-        className="text-center text-4xl font-medium leading-tight text-foreground sm:text-5xl md:text-7xl"
+        className="text-center text-4xl font-medium leading-tight text-foreground sm:text-5xl md:text-8xl"
         initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
         animate={{ filter: "blur(0px)", opacity: 1, y: 0 }}
         transition={{
