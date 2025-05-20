4ac4bfd5e7f70a7f0e3f804bf5ae974f5e5f3a92
Clement Peleman <clementpeleman@outlook.com>
2025-05-20 08:36:33 +0200

    changed hiw content

--- src/components/sections/how-it-works.tsx
+++ src/components/sections/how-it-works.tsx
@@ -7,7 +7,7 @@ const data = [
     id: 1,
     title: "1. Upload Your Menu",
     content:
-      "Easily upload your menu or input individual dishes into our secure platform. We support various formats to ensure a seamless experience.",
+      "Upload your menu or input individual dishes.",
     image: "/dashboard.png",
     icon: <Upload className="w-6 h-6 text-primary" />,
   },
@@ -15,7 +15,7 @@ const data = [
     id: 2,
     title: "2. Get Wine Pairings",
     content:
-      "Our advanced AI algorithms analyze your menu and recommend the perfect wine pairings, tailored to your dishes and preferences.",
+      "AI algorithms analyze your menu and recommend wine pairings.",
     image: "/dashboard.png",
     icon: <Zap className="w-6 h-6 text-primary" />,
   },
@@ -23,7 +23,7 @@ const data = [
     id: 3,
     title: "3. Save and Order",
     content:
-      "Save your selected wines to your dashboard and place orders directly from the platform for a hassle-free experience.",
+      "Save your selected wines and place orders directly from the platform.",
     image: "/dashboard.png",
     icon: <Sparkles className="w-6 h-6 text-primary" />,
   },
